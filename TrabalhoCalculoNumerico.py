df042aec25b44517e95d92e7cb5d107b541a2ab0
Breno Costa <breno.costa182@gmail.com>
2018-10-01 20:12:45 -0300

    Adicionando lógica metodo de Newton-Raphson aplicado ao trabalho

---
+++ TrabalhoCalculoNumerico.py
@@ -0,0 +1,24 @@
+from math import *
+
+def concentracaoDeOD(t):
+    return 9-7*(exp(-0.88*t))
+
+def derivadaConcentracaoDeOD(t):
+    return 6.16*(exp(-0.88*t))
+
+def funcaoDeIteracao(t0):
+    return t0-(concentracaoDeOD(t0) / derivadaConcentracaoDeOD(t0))
+
+def metodoDeNewtonParaOCaso(t0,valorErro):
+    tempoAnterior = t0
+    tempoSeguinte = 0
+    for contador in range (100):
+        if (abs(funcaoDeIteracao(t0)) <=  valorErro):
+            tempoSeguinte = tempoAnterior 
+            break
+        tempoSeguinte = funcaoDeIteracao(tempoAnterior)
+        if ((tempoSeguinte - tempoAnterior ) < valorErro ):
+            break
+        tempoAnterior = tempoSeguinte
+    return abs(tempoSeguinte)
+            
