3b98d182b9e61ac5f98f92e4cffc058d3f482b5b
Breno Costa <breno.costa182@gmail.com>
2018-10-16 00:21:39 -0300

    Alterando Função para estado final

--- TrabalhoCalculoNumerico.py
+++ TrabalhoCalculoNumerico.py
@@ -1,7 +1,9 @@
 from math import *
 
+erro = 0.00001
+
 def concentracaoDeOD(t):
-    return 9-7*(exp(-0.88*t))
+    return 1-7*(exp(-0.88*t))
 
 def derivadaConcentracaoDeOD(t):
     return 6.16*(exp(-0.88*t))
@@ -13,6 +15,8 @@ def metodoDeNewtonParaOCaso(t0,valorErro):
     tempoAnterior = t0
     tempoSeguinte = 0
     for contador in range (100):
+        if derivadaConcentracaoDeOD(tempoAnterior) == 0 :
+            break
         if (abs(funcaoDeIteracao(t0)) <=  valorErro):
             tempoSeguinte = tempoAnterior 
             break
